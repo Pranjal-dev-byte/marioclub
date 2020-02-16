2f6e54c3cd4c8aeea4fbacd7f75acc5b10d57e40
pranjal <pranjalmisra2410@gmail.com>
2020-02-16 10:07:46 +0530

    Club Website, added the signup, login forms via django

---
+++ func/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# class User(models.Model):
+
+#   First=models.CharField(max_length=200)
+#  Last=models.CharField(max_length=200)
+# email=models.EmailField
